ed017f5246e6df1b9661b571e5add6f709fa7f87
Harsaka <redder710@gmail.com>
2019-11-01 17:45:10 +0900

    add gcd of polynomial

--- functions_day7.cpp
+++ functions_day7.cpp
@@ -4,6 +4,12 @@ using namespace std;
 #define InputIterator typename
 #define Semiring typename
 
+
+// 多項式の計算 (Σa_i × x*x*...*x)
+// x にはどんな構造を乗せられるか
+// 加算(+) が定義されている(c = b1 + b2)
+// 定数倍(×) が可能
+// 乗算(*) が定義されている
 template <InputIterator I, Semiring R>
 R polynomial_value(I first, I last, R x) {
     if (first == last) return R(0);
@@ -14,13 +20,24 @@ R polynomial_value(I first, I last, R x) {
     }
     return sum;
 }
-int main(){
+
+void test_polynomial_value() {
     int n;
     cin >> n;
     vector<int> a(n);
     for (int i = 0; i < n; i++) cin >> a[i];
     int res = polynomial_value(a.begin(), a.end(), 3);
     cout << res << endl;
-    cout << "" << endl;
+    return;
+}
+
+polynomial<real> gcd(polynomial<real> a, polynomial<real> b) {
+    while (b != polynomial<real>(0)) {
+        a = remainder(a, b);
+        swap(a, b);
+    }
+    return a;
+}
+int main(){
     return 0;
 }
